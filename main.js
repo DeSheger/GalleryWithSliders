e18a1cc1972c52ac5a2f63a5e23f5ec08bfaa0f4
DeSheger <pgsteam@interia.pl>
2021-11-16 23:12:17 +0100

    Gallery are complete

--- main.js
+++ main.js
@@ -10,7 +10,8 @@ import menu from './modules/menu.js';
 import textAnimation from './modules/animations.js';
 // Slider
 import Sliders from './modules/sliders.js';
-
+// Gallery
+import Gallery from './modules/gallery.js';
 // DOM //
 // MENU
 
@@ -34,24 +35,37 @@ window.addEventListener('scroll', aboutAnimation, false);
 // SLIDERS
 const sliders = new Sliders(slidersImages, slidersRadio);
 
+const autoInterval = setInterval(() => {
+  sliders.nextImageButton();
+  sliders.inputChange();
+}, 2500);
+
 sliders.sliderReset();
 
 document.querySelector('.sliders__prev').addEventListener('click', () => {
   sliders.prevImageButton();
   sliders.inputChange();
+  clearInterval(autoInterval);
 });
 
 document.querySelector('.sliders__next').addEventListener('click', () => {
   sliders.nextImageButton();
   sliders.inputChange();
+  clearInterval(autoInterval);
 });
 
 slidersRadio.forEach((radio) => {
   radio.addEventListener('click', () => {
     sliders.changeImageByInput(radio.value);
+    clearInterval(autoInterval);
   });
 });
 
 // GALLERY
 galleryImages.forEach((image) => {
+  image.addEventListener('click', () => {
+    const gallery = new Gallery(image);
+    gallery.showImage();
+    gallery.closeImage();
+  });
 });
---
+++ modules/gallery.js
@@ -0,0 +1,47 @@
+class Gallery {
+  constructor(image) {
+    this.image = image;
+    this.container = image.parentNode;
+    this.gallery = this.container.parentNode;
+    this.button = document.querySelector('.gallery__button');
+    this.images = document.querySelectorAll('.gallery__image');
+  }
+
+  showImage() {
+    this.images.forEach((image) => {
+      const item = image;
+      item.style.display = 'none';
+    });
+
+    this.image.style.display = 'block';
+    this.image.style.width = '100%';
+
+    this.gallery.style.position = 'fixed';
+    this.gallery.style.top = '0';
+    this.gallery.style.left = '0';
+    this.gallery.style.backgroundColor = 'black';
+    this.gallery.style.zIndex = '3';
+    this.button.style.display = 'block';
+  }
+
+  closeImage() {
+    this.button.addEventListener('click', () => {
+      this.images.forEach((image) => {
+        const item = image;
+        item.style.display = 'block';
+        item.style.width = '50%';
+      });
+
+      this.gallery.style.position = 'static';
+      this.gallery.style.top = 'auto';
+      this.gallery.style.left = 'auto';
+      this.gallery.style.backgroundColor = 'white';
+      this.gallery.style.zIndex = '0';
+      this.button.style.display = 'none';
+
+      this.image.scrollIntoView({ block: 'center' });
+    });
+  }
+}
+
+export default Gallery;
