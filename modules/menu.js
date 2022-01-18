c89688504657780e6ab9907f4bed32f48296a7ec
DeSheger <pgsteam@interia.pl>
2022-01-18 14:37:49 +0100

    List media query add

--- modules/menu.js
+++ modules/menu.js
@@ -6,23 +6,27 @@ function moveMenu(bool) {
   const menu = document.querySelector(".list");
   if(bool) {
     menu.style.position = "fixed";
-    menu.style.bottom = "0px";
+    menu.style.width = "100%";
+    menu.style.height = "50px";
+    menu.style.top = "0px";
     menu.style.background = "white";
-    menu.style.padding = "2%";
-    menu.style.borderRadius = "20px";
+    menu.style.padding = "0 10%";
     menu.style.color = "black";
   }
   else {
     menu.style.position = "static";
-    menu.style.bottom = "auto";
+    if(window.matchMedia("(min-width: 800px)").matches) {
+      menu.style.width = "70%";
+    } else {
+      menu.style.width = "100%";
+    }
+    menu.style.height = "auto";
+    menu.style.top = "auto";
     menu.style.background = "none";
     menu.style.padding = "5%";
     menu.style.borderRadius = "none";
     menu.style.color = "#ececec";
   }
-  
-
-  
 }
 
 export {menu, moveMenu};
